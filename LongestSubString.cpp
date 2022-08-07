fa18fba442be65c3c066cfe1fb59498a948919a3
oshan devinda <oshan.devinda35@gmail.com>
2022-08-07 05:52:27 +0530

    Longest substring solution

---
+++ LongestSubString.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+#include<string.h>
+#include <algorithm>
+
+//abca
+int lengthOfLongestSubString(string s){
+    int length = s.length();
+    int longestLength = 0;
+    //string longestString = "";
+    for(int i=0;i<length;i++){
+        for(int j=1;j<=length;j++){
+            string sub;
+            if(i+j <= length){
+                sub = s.substr(i,j);
+            }else{
+                break;
+            }
+            bool canuse=true;
+
+            for(int letter=0;letter<sub.length();letter++){
+                size_t n= count(sub.begin(),sub.end(),sub[letter]);
+                if(n>1){
+                    canuse = false;
+                    break;
+                }
+            }
+
+  
+            if(longestLength<sub.length() && canuse){
+                longestLength = sub.length();
+                //longestString = s.substr(i,j);
+            }
+        }
+
+    }
+
+    return longestLength;
+}
+
+int main(){
+    //string test="abca";
+    //cout<<test.substr(2,1);
+    lengthOfLongestSubString("bbbbb");
+}
