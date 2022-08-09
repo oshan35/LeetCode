c6cbb9d3e40758ed50cf1e0b4b773a27d04aac24
oshan devinda <oshan.devinda35@gmail.com>
2022-08-09 16:11:24 +0530

    Miner changes done

--- LongestSubString.cpp
+++ LongestSubString.cpp
@@ -16,17 +16,20 @@ int lengthOfLongestSubString(string s){
     //string longestString = "";
     int root_pos = 0;
     while(root_pos<length){
-        vector<char> subString;
-        subString.push_back(s[root_pos]);
+        if(longestLength>(length-root_pos)){
+            break;
+        }
+        string subString;
+        subString+=s[root_pos];
         for(int j=root_pos+1;j<=length+1;j++){
             //cout<<subString.at(0)<<endl;
             if(!(count(subString.begin(),subString.end(),s[j]))){
-                subString.push_back(s[j]);
+                subString+=s[j];
             }else{
                 root_pos = j;
-                if (longestLength<subString.size())
+                if (longestLength<subString.length())
                 {
-                    longestLength = subString.size();
+                    longestLength = subString.length();
                 }
                 
                 break;
