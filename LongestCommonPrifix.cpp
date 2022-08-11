5512d803854dff90a7f737f38eef1e67009c6f5f
oshan devinda <oshan.devinda35@gmail.com>
2022-08-11 13:58:39 +0530

    Longest common prefix

---
+++ LongestCommonPrifix.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+string longestCommonPrefix(vector<string>& strs) {
+    string prefix = strs.at(i);
+    
+      
+}
+
+int main(){
+
+    return 0;
+}
