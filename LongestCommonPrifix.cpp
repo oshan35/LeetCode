7c72e7b11001f8435f558f49931c8b6e9a3cb319
oshan devinda <oshan.devinda35@gmail.com>
2022-08-13 08:21:15 +0530

    Longest common prifix solution

--- LongestCommonPrifix.cpp
+++ LongestCommonPrifix.cpp
@@ -3,12 +3,46 @@ using namespace std;
 #include <vector>
 
 string longestCommonPrefix(vector<string>& strs) {
-    string prefix = strs.at(i);
-    
-      
+    bool common = true;
+    string commonPrefix = "";
+    int index= 0;
+    while(common){
+        if (strs.at(0).length() == index)
+        {
+            break;
+        }
+        
+        char ch = strs.at(0)[index];
+        for (int i = 1; i < strs.size(); i++)
+        {   
+            string word = strs.at(i);
+            if (word.length() == index)
+            {
+                common = false;
+                break;
+            
+            }
+            
+            char ch2 =  word[index];
+            if (ch2!=ch)
+            {
+                common = false;
+            }
+            
+        }
+        if (common)
+        {
+            commonPrefix+=ch;
+        }
+        index++;
+        
+    }
+    return commonPrefix;  
 }
 
 int main(){
-
+    vector<string> test = {"ab","a"};
+    string commonPrifix = longestCommonPrefix(test);
+    cout<<"Longest common prifix: "<<commonPrifix;
     return 0;
 }
