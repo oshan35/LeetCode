751e99301e068f2b38ce737ace4b897b700e3088
oshan devinda <oshan.devinda35@gmail.com>
2022-08-15 18:14:36 +0530

    solution for valid parantheses

--- ValidParentheses.cpp
+++ ValidParentheses.cpp
@@ -1,26 +1,54 @@
-#include <stdio.h>
+#include <iostream>
 #include <string>
 #include <map>
-#include <iostream>
 using namespace std;
+// "()[]{}"
+bool isValid(string s){
+    std::map<char, int> dict = {{'(',1},{'[',2},{'{',3},{'}',4},{']',5},{')',6}};
 
-int main()
-{
-    string s="()[]{}";
-    std::map<char, char> dict = {{'(','1'},{'[','2'},{'{','3'},{'}','4'},{']','5'},{')','6'}};
+    bool valid = true;
+    string map = "";
 
-    string map ="";
-
-    for (int  i = 0; i < s.length(); i++)
+    for (int i = 0; i < s.length(); i++)
     {
-        map+=dict[s[i]];
+       int num = dict[s[i]];
+       if(num<4){
+            map+=std::to_string(num);
+       }else if (num>=4)
+       {
+            //char prev_char[map[0].length()] = map[0];
+            int index =map.length()-1;
+            
+            int prev = map[index]-'0';
+            {
+                /* code */
+            }
+            
+            if (prev+num == 7)
+            {
+                map.erase(index,1);
+            }else{
+                valid = false;
+                break;
+            }
+            
+            
+       }
+       
     }
+    if (map.length()!=0)
+    {
+        valid = false;
+    }
+    
 
-    for(int j=0;j<map.length();j++){
-        int pos = (int) map[j];
-        cout<<pos<<endl;
-    }
+    return valid;
     
-    
+};
+
+int main(){
+    string s ="]";
+    bool answer=isValid(s);
+    cout<<answer<<endl;
     return 0;
 }
