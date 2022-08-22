efd243b68357ce7152ec5ffec3fd2842b8e9a2e7
oshan devinda <oshan.devinda35@gmail.com>
2022-08-22 23:39:33 +0530

    list node start

---
+++ MergeTwoSortedLists.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
+    
+}
+
+ListNode* createLinkedList(int list[]){
+    if(sizeof(list)/sizeof(list[0])==0){
+        return
+    }
+
+    return ListNode()
+}
+
+
+int main(){
+    int list1[] = {1,2,4};
+    int list2[] = {1,3,4};
+
+
+    return 0;
+}
