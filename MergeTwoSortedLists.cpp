6b7b5732077599aa59726ffdd519dd871979a7b5
oshan devinda <oshan.devinda35@gmail.com>
2022-08-24 21:03:45 +0530

    merge two sorted lists solution

--- MergeTwoSortedLists.cpp
+++ MergeTwoSortedLists.cpp
@@ -10,22 +10,55 @@ struct ListNode {
 };
 
 ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
-    
-}
 
-ListNode* createLinkedList(int list[]){
-    if(sizeof(list)/sizeof(list[0])==0){
-        return
+    if (!list1)
+    {
+        return list2;
+    }
+    if (!list2)
+    {
+        return list1;
+    }
+    
+    if (list1->val<list2->val)
+    {
+        list1->next = mergeTwoLists(list1->next,list2);
+        return list1;
+    }else
+    {
+        list2->next = mergeTwoLists(list1,list2->next);
+        return list2;
     }
 
-    return ListNode()
+
+}
+
+// ListNode* createLinkedList(int list[]){
+//     if(sizeof(list)/sizeof(list[0])==0){
+//         return
+//     }
+
+//     return ListNode()
+// }
+
+void printList(ListNode* node)
+{
+    while (node != NULL) 
+    {
+        printf("%d  ", node->val);
+        node = node->next;
+    }
 }
 
 
 int main(){
-    int list1[] = {1,2,4};
-    int list2[] = {1,3,4};
+    ListNode *linkedList1;
+    linkedList1=new ListNode(1,new ListNode(2, new ListNode(5)));
+    ListNode *linkedList2;
+    linkedList2=new ListNode(1,new ListNode(3,new ListNode(4)));
 
-
+    ListNode *result=mergeTwoLists(linkedList1,linkedList2);
+    
+    printList(result);
     return 0;
 }
