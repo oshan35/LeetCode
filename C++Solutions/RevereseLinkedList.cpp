e0037e2e851a5a3659c1aaca0078dcd74353c05b
oshan35 <oshan.20191052@iit.ac.lk>
2022-09-24 04:27:26 +0530

    reverse linkedlist update

---
+++ C++Solutions/RevereseLinkedList.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* reverse(ListNode* head){
+    ListNode* temp =NULL;
+    ListNode* prev=NULL;
+    ListNode* curr = head;
+    while (curr!=NULL)
+    {
+        temp = curr->next;
+        curr->next =prev;
+        curr = prev;
+
+        
+
+    }
+    head = prev;
+
+    return head;
+    
+}
+
+
+
+void printList(ListNode* node)
+{
+    while (node != NULL) 
+    {
+        printf("%d  ", node->val);
+        node = node->next;
+    }
+}
+
+
+int main(){
+    ListNode *linkedList1;
+    linkedList1=new ListNode(1,new ListNode(2, new ListNode(4, new ListNode(5))));
+    ListNode *linkedList2;
+    linkedList2=new ListNode(1,new ListNode(3,new ListNode(4)));
+
+    ListNode *result=reverse(linkedList1);
+    
+    printList(result);
+    return 0;
+}
