1cf1f10c84f59e0d8b09995670b6718c4c406393
oshan35 <oshan.20191052@iit.ac.lk>
2022-10-04 18:07:44 +0530

    big button solution

--- C++Solutions/InorderTraversal.cpp
+++ C++Solutions/InorderTraversal.cpp
@@ -1,75 +1,63 @@
+// C++ program to print inorder traversal
+// using stack.
 #include<iostream>
-using namespace std;
-#include<vector>
 #include<stack>
+using namespace std;
 
+/* A binary tree Node has data, pointer to left child
+and a pointer to right child */
+struct Node
+{
+	int data;
+	struct Node* left;
+	struct Node* right;
+	Node (int data)
+	{
+		this->data = data;
+		left = right = NULL;
+	}
+};
 
-struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode() : val(0), left(nullptr), right(nullptr) {}
-      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
+/* Iterative function for inorder tree
+traversal */
+void inOrder(struct Node *root)
+{
+    stack <Node*> nums;
+    Node* current=root;
+    nums.push(current);
+    while(current!=NULL || !(nums.empty())){  
+        while(current!=NULL){ 
+            cout<<current->data<<" ";
+            nums.push(current);
+            current=current->left;           
+            
+        }    
+        current=nums.top();                    
+        nums.pop();
 
- vector<int> inorderTraversal(TreeNode* root){
-    vector<int> indorList;
-
-    stack<TreeNode*> stack;
-
-    //pair<TreeNode*,int> n1;
-    // n1.first = root;
-    // n1.second = 1;
-
-    stack.push(root);
-
-    while (!stack.empty()){
-        TreeNode* curr = stack.top();
-
-        stack.pop();
-
-        indorList.push_back(curr->val);
-
-
-        if(curr->left!=NULL){
-            stack.push(curr->left);
-        }
-        
-        if (curr->right!=NULL){
-            stack.push(curr->right);
-        }
-
-
-
-        
-
-    }
-
-    return indorList;
- }
-
- TreeNode* createNode(int val){
-    TreeNode* newNode = new TreeNode(val);
-    return newNode;
- }
-
- int main(){
-    TreeNode* root= new TreeNode(1);
-
-    root->left= nullptr;
-    root->right= createNode(2);
-    root->right->left = createNode(3);
-
-    vector<int> result = inorderTraversal(root);
-
-    for (int i = 0; i < result.size(); i++)
-    {
-        cout<<result[i]<<" ";
-    }
+        nums.push(current);        
+        current=current->right;   
     
+    }
+}
 
+/* Driver program to test above functions*/
+int main()
+{
 
+	/* Constructed binary tree is
+			1
+			/ \
+		2	 3
+		/ \
+	4	 5
+	*/
+	struct Node *root = new Node(1);
+	root->left	 = new Node(2);
+	root->right	 = new Node(3);
+	root->left->left = new Node(4);
+	root->left->right = new Node(5);
 
- }
-
+	inOrder(root);
+	return 0;
+}
