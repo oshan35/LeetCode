39d6f51a00d77e8b1044186cbeda849777362399
oshan35 <oshan.20191052@iit.ac.lk>
2022-11-20 13:33:24 +0530

    superhero solution

--- PythonSolutions/CodeSquad/superhero.py
+++ PythonSolutions/CodeSquad/superhero.py
@@ -1,83 +1,81 @@
-
-
-
-#[1, 2, 3, 1, 1, 2, 3, 1, 1]
-def maxPic(list):
-    max_size =0
-    cat_count ={}
+def count(list):
+    dict = {}
 
     for i in range(len(list)):
-        if list[i]-1 == -1:
-            continue
-        if list[i]-1 not in cat_count.keys():
-            cat_count[list[i]-1] = 1
-        else:
-            cat_count[list[i]-1] = cat_count[list[i]-1]+1
-        
- 
-    p_left = 0
-    p_right =len(list)-1
-    min_cat = cat_count[min(cat_count.values())]
-
-    while p_left<p_right:
-        num_left = list[p_left]
-        num_right = list[p_right]
-
-        if num_left != min_cat:
-            
-
-def maxPhoto(list,k):
-    max_size = 0
-    count_from_each_cat = {} #{1:1, 2: 1, 3: 1}
-
-    for i in range(len(list)):
-        if list[i]-1 == -1:
+        if list[i] == 0:
             continue
 
-        if list[i]-1 not in count_from_each_cat.keys():
-            count_from_each_cat[list[i]-1] = 1
+        if list[i] not in dict.keys():
+            dict[list[i]] = 1
         else:
-            count_from_each_cat[list[i]-1] = count_from_each_cat[list[i]-1]+1
+            dict[list[i]]+=1
+
+    return dict
         
-        flag = True
+
+def max_size(list):
+    max_size = -1
+    left_pt = 1
+    right_pt = len(list)-1
+
+    while left_pt<right_pt:
+        if list[left_pt] == 0:
+            left_pt+=1
+            continue
+        elif list[right_pt] == 0:
+            right_pt-=1
+            continue
+
+        counts = count(list[left_pt:right_pt+1])
         prev = None
-        for key in count_from_each_cat.keys():
+        flag = True
+        for c in counts.keys():
             if prev == None:
-                prev = count_from_each_cat[key]
+                prev= counts[c]
                 continue
-            if prev != count_from_each_cat[key]:
+            if prev > counts[c]:
+                prev = counts[c]
                 flag = False
-                break
-            
-        
-        if not flag:
-            count_from_each_cat = {}
-            count_from_each_cat[list[i]-1] = 1
+
+                        
+        cat_left, cat_right = list[left_pt], list[right_pt]
+
+        if flag and len(counts.keys())>=2:
+            size = right_pt - left_pt
+            if size>max_size:
+                max_size = size
+            left_pt+=1
+
+
+        if counts[cat_right] > prev:
+            right_pt-=1
+        elif counts[cat_left] > prev:
+            left_pt+=1
         else:
-            new_max = max(count_from_each_cat.keys())- min(count_from_each_cat.keys())
-            if new_max>max_size:
-                max_size = new_max
-    return max_size
-
+            left_pt+=1
+    print(max_size)
         
 
 
-# nums = [int(x) for x in input().split(" ")]
+
+
+# nums = [int(x) for x in input().split()]
 # n,k = nums[0],nums[1]
-n,k =9,2
+# # n,k =9,2
 
 # maxi = 0
 # dict ={}
 # for i in range(n):
-#     ith = [int(x) for x in input().split(" ")]
+#     ith = [int(x) for x in input().split()]
 #     dict[ith[0]] = ith[1]
 #     if ith[0]>maxi:
 #         maxi = ith[0]
-dict = {1:1,5:1,6:1,9:1,100:1,2:2,7:2,3:3,8:3}
+# dict = {1:1,5:1,6:1,9:1,100:1,2:2,7:2,3:3,8:3}
+dict = {0:1,1:2,2:2,3:3,4:3,5:3,6:3,7:2,8:2,9:1}
 maxi = max(dict.keys())
 
 list = [0]*(maxi+1)
 for key, val in dict.items():
-    list[key-1] = val
+    list[key] = val
 
-print(maxPhoto(list,k))
+max_size(list)
